da24d9d8b210517042423a47cd537f2b6f98d252
Joseph Morales Silva <josephzx1@hotmail.com>
2021-07-04 20:07:10 -0600

    nav 404

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <nav className="container my-5 ">
+          <ul>
+            <li className="font-Inconsolata text-2xl uppercase ml-6">
+              404 not found
+            </li>
+          </ul>
+        </nav>
+      </Fragment>
+    );
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      Inconsolata: ["Inconsolata"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
